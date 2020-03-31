08c230459f2cf0d3eba686be5eb83b5ac582d338
mchtalkn <mucahit.a.yorganci@gmail.com>
2020-04-01 02:26:57 +0300

    ikinci fonksiyonu da tokatladık

--- HW2.hs
+++ HW2.hs
@@ -18,6 +18,7 @@ foldAndPropagateConstants a = myFNP (reverse a) []
 foldAndPropagateConstants _ = []
 
 assignCommonSubexprs :: ExprV -> ([(String, ExprV)], ExprV)
+assignCommonSubexprs a= assignHelper [] a 0
 assignCommonSubexprs _ = ([], notImpl)
 
 reducePoly :: ExprV -> ExprV
@@ -59,4 +60,34 @@ isIncludes :: Eq a=> [a]->a->Bool
 isIncludes [] _ =False
 isIncludes (x:y) a= if x==a then True else isIncludes y a
 
-findCommonLeaves ExprV->[(String,ExprV)]->[(String,ExprV)]->[(String,ExprV)]
+findCommons :: Eq a=> [a]->[a]->[a]
+findCommons []  b = b
+findCommons (x:y) b = if( isIncludes y x) then findCommons (filter (/=x) y) (x:b)  else findCommons y b
+
+isLeaf :: ExprV->Bool
+isLeaf (Leaf ( Constant _))=True
+isLeaf (Leaf (Variable _))=True
+isLeaf _ = False
+
+findBigLeaves :: ExprV->[ExprV]->[ExprV]
+findBigLeaves (UnaryOperation Minus a) l =if (isLeaf a) then ((UnaryOperation Minus a):l) else findBigLeaves a l
+findBigLeaves (BinaryOperation Plus a b) l =if ((isLeaf a)&& (isLeaf b)) then ((BinaryOperation Plus a b):l) else ((findBigLeaves a l)++(findBigLeaves b l))++l
+findBigLeaves (BinaryOperation Times a b) l =if ((isLeaf a)&& (isLeaf b)) then ((BinaryOperation Times a b):l) else ((findBigLeaves a l)++(findBigLeaves b l))++l
+findBigLeaves _ l=l
+
+namer :: [ExprV]->Int->Int->[(String,ExprV)]
+namer [] _ _ = []
+namer (x:l) m n= (((("&"++(show m))++(show n)),x) : (namer l m (n+1)))
+
+assignHelper :: [(String,ExprV)]-> ExprV -> Int-> ([(String, ExprV)], ExprV)
+assignHelper l a m =if ((length (findCommons (findBigLeaves a []) []))==0) then (l,a) else let k=(namer (findCommons (findBigLeaves a []) []) m 0) in assignHelper (l++k) (recAssign k a) (m+1)
+
+assign ::(String,ExprV)->ExprV->ExprV
+assign (s,v) (UnaryOperation Minus a)= if((UnaryOperation Minus a)==v) then Leaf(Variable s) else UnaryOperation Minus (assign (s,v) a)
+assign (s,v) (BinaryOperation Plus a b)=if((BinaryOperation Plus a b)==v) then Leaf (Variable s) else BinaryOperation Plus (assign (s,v) a) (assign (s,v) b)
+assign (s,v) (BinaryOperation Times a b)=if((BinaryOperation Times a b)==v) then Leaf (Variable s) else BinaryOperation Times (assign (s,v) a) (assign (s,v) b)
+assign _ a=a
+
+recAssign:: [(String,ExprV)]->ExprV->ExprV
+recAssign [] a=a
+recAssign (x:y) a=recAssign y (assign x a)
