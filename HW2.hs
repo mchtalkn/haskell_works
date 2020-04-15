ddae04bcf8ba2c90d37fbbc0e21fa1abc7216f31
mchtalkn <mucahit.a.yorganci@gmail.com>
2020-04-15 19:44:16 +0300

    bugfix about negative constants

--- HW2.hs
+++ HW2.hs
@@ -43,6 +43,8 @@ replace ((x,val),BinaryOperation Times a b)= BinaryOperation Times (replace((x,v
 isle :: ExprV->ExprV
 isle (Leaf (Constant a)) =Leaf (Constant a)
 isle  (UnaryOperation Minus (Leaf (Constant 0)))=Leaf (Constant 0)
+isle (UnaryOperation Minus (Leaf (Constant a)))=Leaf (Constant( -1*a))
+isle (UnaryOperation Minus (UnaryOperation Minus (Leaf (Variable a))))=Leaf (Variable a)
 isle (UnaryOperation Minus (UnaryOperation Minus (Leaf (Constant a))))=Leaf (Constant a)
 isle (BinaryOperation Plus (Leaf (Constant a)) (Leaf (Constant b))) =Leaf (Constant (a+b))
 isle (BinaryOperation Times (Leaf (Constant a)) (Leaf (Constant b)))=Leaf (Constant (a*b))
